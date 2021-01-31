038d199299892b9732f59b84956911a54c950a7c
Alyssa <williamsalyssa1@gmail.com>
2021-01-31 03:31:13 -0500

    Most updated version

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+//import styles from "../styles/globals.css"
 import axios from "axios";
 import React from "react";
 
@@ -127,8 +128,79 @@ function add2(name) {
   foo.appendChild(element);
 }
 
-  return(
+return(
+  <main>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      
+      <title>SnailzLibz</title>
+      <img 
+        rel="shortcut icon" 
+        href="https://discord.com/channels/@me/805089351276953640/805282609541939271"
+        type="image/x-icon" 
+        ></img>
+      <link rel="stylesheet" type="text/css" href="syntax.css"></link>      
+    </head>
+
+    <body>
+    <div className = "bg-black h-screen text-white">
+      <div>
+        <img src="https://cdn.discordapp.com/attachments/805089351276953640/805264804402167828/snail_white.png" align="left" vspace="5" width="150" height="150" alt="Snailz"/>
+        <h1 className = "font-bold text-8xl text-white">SnailzLibz</h1>
+      </div>
+      <div className = "md:container md:mx-auto py-10">
     
-  )
-  
+      <form onSubmit={onSubmit}>
+        <h1 width="100" vspace="50" className = "text-center" align="left">We love Snailz</h1>
+      <section class="text-center">
+        <button
+          rel="noopener noreferrer"
+          target="_blank"
+          type="submit"
+          class="mt-10 bg-white rounded-lg px-8 py-4 text-black inline-block">Make me a SnailzLibz!</button>
+        </section>
+        {/* <label htmlFor="thisone">Enter Here:</label> */}
+        {/* <input class="bg-black" id="thisone">
+        </input> */}
+        <h1 className = "text-white">{}</h1>
+        <p className = "text-white" id='demo'>{}</p>
+        </form>
+
+        <div id="holder" class="my-5">
+          <div id = "story" class={storyVisibility}>
+            
+          </div>
+        </div>
+
+        {/* <div class="invisible">*/}
+        <div>
+        {/*<form onSubmit={this.handleSubmit}>
+        <section class="text-center">
+        <button
+          rel="noopener noreferrer"
+          target="_blank"
+          type="submit"
+          class="mt-10 bg-white rounded-lg px-8 py-4 text-black inline-block"
+          onClick="">
+            Submit</button>
+        </section>
+      </form>*/}
+          
+        </div>
+      <section class="text-center">
+        <button
+          rel="noopener noreferrer"
+          target="_blank"
+          type="button"
+          class="mt-10 bg-white rounded-lg px-8 py-4 text-black inline-block"
+          onClick={changeVisibility}
+          >Submit Here!</button>
+      </section>
+        
+      </div>
+    </div>
+  </body>
+  </main>
+)
 }
